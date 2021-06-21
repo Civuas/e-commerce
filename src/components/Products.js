8358fc80613d2ef0de098b1ea39e3d0b1536c9da
Naveengali <naveengali80@gmail.com>
2021-06-21 16:10:59 +0530

    Added Items to Cart

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { Button } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Alert, Button } from 'rsuite';
+import { useCartDispatch } from '../libs/CartContext';
 
 const Products = ({ products }) => {
+  const dispatchCart = useCartDispatch();
+
+  const handleAddToCart = useCallback(
+    (id, price, inStock) => {
+      if (!inStock) {
+        return;
+      }
+      dispatchCart({ type: 'ADD_ONE', id, price });
+      Alert.info('Item Added To Cart', 2000);
+    },
+    [dispatchCart]
+  );
+
   if (products.length === 0) {
     return <div>No Products Found</div>;
   }
+
   return products.map(
     ({
       currency,
@@ -23,7 +38,11 @@ const Products = ({ products }) => {
         </div>
         <div>{inStock ? 'In Stock' : 'Out of Stock'}</div>
         {delivery && <div>Delivery Avaliable</div>}
-        <Button appearance="ghost" disabled={!inStock}>
+        <Button
+          appearance="ghost"
+          disabled={!inStock}
+          onClick={() => handleAddToCart(restOfProduct.id, price, inStock)}
+        >
           Add to Cart
         </Button>
       </div>
