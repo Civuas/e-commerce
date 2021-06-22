0f802cd7f1a4b7a30891ff8e82be17e06c337bd1
Naveengali <naveengali80@gmail.com>
2021-06-22 19:49:09 +0530

    Products are added to checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,5 +1,71 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useCart, useCartDispatch } from '../libs/CartContext';
+import PRODUCTS from '../db/products.json';
 
-const Checkout = () => <div>Checkout</div>;
+function getComputedCheckedoutItems(products, cartItems) {
+  const productsInCart = products.filter(p =>
+    cartItems.some(item => item.id === p.id)
+  );
+
+  return productsInCart.map(product => ({
+    ...product,
+    quantity: cartItems.find(item => item.id === product.id).quantity,
+  }));
+}
+
+const Checkout = () => {
+  const cart = useCart();
+  const dispatchCart = useCartDispatch();
+  const products = useState(PRODUCTS);
+
+  const checkoutItems = getComputedCheckedoutItems(products, cart);
+
+  const handleAdd = useCallback(
+    id => {
+      dispatchCart({ type: 'ADD_ONE', id });
+    },
+    [dispatchCart]
+  );
+
+  const handleRemoveOne = useCallback(
+    id => {
+      dispatchCart({ type: 'REMOVE_ONE', id });
+    },
+    [dispatchCart]
+  );
+
+  const handleRemove = useCallback(
+    id => {
+      dispatchCart({ type: 'REMOVE', id });
+    },
+    [dispatchCart]
+  );
+
+  return (
+    <div>
+      {checkoutItems.map(el => (
+        <div key={el.id}>
+          <img src={el.thumbnail} alt={el.name} width={30} />
+          <span>{el.name}</span>
+          <span>
+            | {el.currency} {el.price}
+          </span>
+          <span> | </span>
+          <button type="button" onClick={() => handleRemoveOne(el.id)}>
+            -
+          </button>
+          <span>{el.quantity}</span>
+          <button type="button" onClick={() => handleAdd(el.id)}>
+            +
+          </button>
+          <span> | </span>
+          <button type="button" onClick={() => handleRemove(el.id)}>
+            X
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Checkout;
